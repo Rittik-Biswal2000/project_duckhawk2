2fdf8c9c8a5aa8743cc7b06300da6456c81e2b31
AITIK DANDAPAT <aitikdandapat@gmail.com>
2020-05-25 23:11:31 +0530

    Handled exceptions

--- lib/pages/electronics.dart
+++ lib/pages/electronics.dart
@@ -5,6 +5,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
 import 'package:fluttertoast/fluttertoast.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:progress_dialog/progress_dialog.dart';
 import 'package:project_duckhawk/pages/account1.dart';
 import 'package:project_duckhawk/pages/cart.dart';
@@ -12,7 +13,6 @@ import 'package:project_duckhawk/pages/cart2.dart';
 
 import 'package:project_duckhawk/pages/item_info.dart';
 import 'package:project_duckhawk/main.dart';
-import 'package:project_duckhawk/src/loginPage.dart';
 import 'package:project_duckhawk/src/welcomPage.dart';
 
 import 'cart1.dart';
@@ -34,6 +34,44 @@ class _eState extends State<e> {
     pr = new ProgressDialog(context, showLogs: true);
     pr.style(message: 'Please wait...');
 
+    Widget _title() {
+      return RichText(
+        textAlign: TextAlign.center,
+        text: TextSpan(
+            text: 'Duckhawk',
+            style: GoogleFonts.portLligatSans(
+              textStyle: Theme.of(context).textTheme.display1,
+              fontSize: 30,
+              fontWeight: FontWeight.w700,
+              color: Color(0xff104670),
+            ),
+            children: [/*
+            TextSpan(
+              text: 'ev',
+              style: TextStyle(color: Colors.black, fontSize: 30),
+            ),
+            TextSpan(
+              text: 'rnz',
+              style: TextStyle(color: Color(0xff104670), fontSize: 30),
+            ),
+         */ ]),
+      );
+    }
+    Widget _title1() {
+      return RichText(
+        textAlign: TextAlign.center,
+        text: TextSpan(
+          text: 'Currently No shop available!',
+          style: GoogleFonts.portLligatSans(
+            textStyle: Theme.of(context).textTheme.display1,
+            fontSize: 20,
+            fontWeight: FontWeight.w700,
+            color: Color(0xff104670),
+          ),),
+      );
+    }
+
+
     return Scaffold(
       appBar: new AppBar(
           backgroundColor: Color(0xff104670),
@@ -41,24 +79,12 @@ class _eState extends State<e> {
           actions: <Widget>[
             // action button
             IconButton(icon:Icon(Icons.shopping_cart),
-                onPressed: () async {
-                  pr.show();
-                  FirebaseUser user=await FirebaseAuth.instance.currentUser();
-                  pr.hide();
-                  if(user==null){
-                    Navigator.push(context,
-                        MaterialPageRoute(builder: (context) => new lp()));
-                  }
-                  else{
-                    pr.show();
-                    await getcartData();
-                    pr.hide();
-                    Navigator.push(context,
-                        MaterialPageRoute(builder: (context) => new cart2()));
-                  }
-
-                },
-
+              onPressed: () async {
+                pr.show();
+                await getcartData();
+                pr.hide();
+                Navigator.push(context, MaterialPageRoute(builder: (context)=>new cart2()));
+              },
             ),
           ]
         //leading:new Text("hi"),
@@ -92,28 +118,13 @@ class _eState extends State<e> {
             ),
             Expanded(
               flex: 1,
-              child: IconButton(
+              child: IconButton (
                   icon: new Icon(Icons.account_box),
-                  onPressed: () async {
+                  onPressed: () async{
                     pr.show();
-                    FirebaseUser user=await FirebaseAuth.instance.currentUser();
-                    print(user);
+                    await getuac();
                     pr.hide();
-                    if(user==null){
-                      Navigator.push(context,
-                          MaterialPageRoute(builder: (context) => new lp()));
-
-                    }
-                    else{
-                      pr.show();
-                      await getuac();
-                      pr.hide();
-
-                      Navigator.push(context,
-                          MaterialPageRoute(builder: (context) => new acc1()));
-                    }
-
-
+                    Navigator.push(context, MaterialPageRoute(builder: (context)=>new acc1()));
                   }),
             ),
             Expanded(
@@ -122,20 +133,11 @@ class _eState extends State<e> {
                 icon: new Icon(Icons.shopping_cart),
                 onPressed: () async {
                   pr.show();
-                  FirebaseUser user=await FirebaseAuth.instance.currentUser();
+                  await getcartData();
                   pr.hide();
-                  if(user==null){
-                    Navigator.push(context,
-                        MaterialPageRoute(builder: (context) => new lp()));
-                  }
-                  else{
-                    pr.show();
-                    await getcartData();
-                    pr.hide();
-                    Navigator.push(context,
-                        MaterialPageRoute(builder: (context) => new cart2()));
-                  }
-
+                  print("Time taken");
+                  stime=s.elapsedMilliseconds;
+                  Navigator.push(context, MaterialPageRoute(builder: (context)=>new cart2()));
                 },),
             ),
             /*Expanded(
@@ -147,7 +149,12 @@ class _eState extends State<e> {
           ],
         ),
       ),
-      body: Container(
+
+      body: Builder(
+
+    builder: (context){
+    if(len1!=0) {
+        return Container(
         child: new ListView.builder(
           itemCount: len1,
           itemBuilder: (BuildContext context,int index){
@@ -242,9 +249,48 @@ class _eState extends State<e> {
 
 
 
-        ),
+        ));}
+    else
+      {
 
-      ),
+        return new Container(
+          height: MediaQuery.of(context).size.height,
+          child: Stack(
+            children: <Widget>[
+              Container(
+                padding: EdgeInsets.symmetric(horizontal: 20),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Row(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        _title(),
+                      ],
+                    ),
+                    SizedBox(
+                      height: 50,
+                    ),
+                    Row(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        Container(
+                          child: _title1(),
+                        )
+                      ],
+                    ),
+                    //_facebookButton(),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        );
+
+      }}),
     );
   }
 }
